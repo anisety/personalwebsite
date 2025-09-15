4dd26f2248268126ad73b08f116da4180c9dd298
Anish Basety <anishbasety@gmail.com>
2025-09-15 01:19:57 -0500

    Project Functionality

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -129,19 +129,6 @@ const Projects = ({ projects }: ProjectsProps) => {
                     </svg>
                     Code
                   </a>
-                  {project.demo && (
-                    <a
-                      href={project.demo}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="project-link-base project-link-demo"
-                    >
-                      <svg className="project-link-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                      </svg>
-                      Demo
-                    </a>
-                  )}
                 </div>
               </div>
             </motion.div>
